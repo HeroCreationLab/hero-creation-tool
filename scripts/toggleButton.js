27ab7a66e4667c315221aa09f473bd860620a300
Chencen Liang <coco.liang@mail.utoronto.ca>
2020-12-20 00:18:40 -0500

    it's ugly rn don't look

--- scripts/toggleButton.js
+++ scripts/toggleButton.js
@@ -1,3 +1,5 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
 $(document).ready(function(){
   $("#startButton").click(function(){
     not$("#startDiv").hide();
