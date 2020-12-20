8e35ede6b7651019e780b671c35b3118f8650cf5
Chencen Liang <coco.liang@mail.utoronto.ca>
2020-12-19 22:35:53 -0500

    at least it looks nice?

--- scripts/hero-creation-tool.js
+++ scripts/hero-creation-tool.js
@@ -1,27 +1,24 @@
-class heroCreationTools extends Application {
-
- constructor() {
-        super();
-        this._initialize();
-    }
+class HeroCreationTools extends FormApplication {
+    
 }
+
 var link = document.createElement('link');  
 link.rel = 'stylesheet';  
 link.type = 'text/css'; 
-link.href = '../styles/hero-creation.css';  
+link.href = '../styles/hero-creation-tool.css';  
 
 Hooks.on('renderActorSheet', (app, html, data) => { 
    
     if (app.actor.data.type === 'npc') return;
-    if (heroCreationTools === undefined) {
-        heroCreationTools = new heroCreationTools();
-    }
-    //let actorId = data.actor._id;
-    let button = $(`<button class="first"}>Hi</button>`);
+    let actorId = data.actor._id;
+
+    let button = $(`<a class="header-button configure_hero"}>Hero Creation</a>`);
+    configure_hero = new HeroCreationTools(actorId, ["",""]);
+
     button.click(ev =>{
         console.log("button pressed!");
     });
-    let titleElement = html.closest('.app').find('.window-title');
-    button.insertAfter(titleElement);
+    let titleElement = html.closest('.app').find('.configure-sheet');
+    button.insertBefore(titleElement);
 
 });
