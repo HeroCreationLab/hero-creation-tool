f5c412140976827bc8bbd3a070b6eed14f0213c8
Chencen Liang <coco.liang@mail.utoronto.ca>
2020-12-19 19:38:07 -0500

    there is functionality on the .click

--- scripts/hero-creation-tool.js
+++ scripts/hero-creation-tool.js
@@ -4,18 +4,24 @@ class heroCreationTools extends Application {
         super();
         this._initialize();
     }
-
-   
-
 }
+var link = document.createElement('link');  
+link.rel = 'stylesheet';  
+link.type = 'text/css'; 
+link.href = '../styles/hero-creation.css';  
 
 Hooks.on('renderActorSheet', (app, html, data) => { 
+   
     if (app.actor.data.type === 'npc') return;
     if (heroCreationTools === undefined) {
         heroCreationTools = new heroCreationTools();
     }
-    //   let actorId = data.actor._id;
-    let button = $(`<button >Hi </button>` + data.actor._id);
+    //let actorId = data.actor._id;
+    let button = $(`<button class="first"}>Hi</button>`);
+    button.click(ev =>{
+        console.log("button pressed!");
+    });
     let titleElement = html.closest('.app').find('.window-title');
     button.insertAfter(titleElement);
+
 });
