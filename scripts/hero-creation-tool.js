51b1b9c373bea3f50afead171209b35bd324d42f
Chencen Liang <coco.liang@mail.utoronto.ca>
2020-12-19 22:53:17 -0500

    pop-up window appears! hurrah

--- scripts/hero-creation-tool.js
+++ scripts/hero-creation-tool.js
@@ -1,5 +1,22 @@
-class HeroCreationTools extends FormApplication {
+class HeroCreationTools extends Application {
     
+    constructor() {
+        super();
+    }
+
+    static get defaultOptions() {
+        const options = super.defaultOptions;
+        options.template = "modules/hero-creation-tool/templates/app.html";
+        options.width = 'auto';
+        options.height = 'auto';
+        options.title = "Hero Creation";
+        return options;
+    }
+
+    async openForActor(actorId){
+        this.actorId = actorId;
+        this.render(true);
+    }
 }
 
 var link = document.createElement('link');  
@@ -12,11 +29,12 @@ Hooks.on('renderActorSheet', (app, html, data) => {
     if (app.actor.data.type === 'npc') return;
     let actorId = data.actor._id;
 
+    configure_hero = new HeroCreationTools();
+    
     let button = $(`<a class="header-button configure_hero"}>Hero Creation</a>`);
-    configure_hero = new HeroCreationTools(actorId, ["",""]);
-
     button.click(ev =>{
         console.log("button pressed!");
+        configure_hero.openForActor(actorId);
     });
     let titleElement = html.closest('.app').find('.configure-sheet');
     button.insertBefore(titleElement);
