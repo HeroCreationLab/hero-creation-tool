70db1442ebe1f88d4329a70697b3cc0215e4910f
Iñaki Guastalli (ccjmk) <iaguastalli@gmail.com>
2020-12-29 03:55:39 +0000

    point buy fixed (had a bug where you maxed one skill, then decreased another, would allow you to keep increasing the maxed one) and code refactored, plus added a missing check for missing abilities (only checked all 6 where different, but the Select Ability option was counted too)

--- scripts/hero-creation-tool.js
+++ scripts/hero-creation-tool.js
@@ -28,6 +28,9 @@ class HeroCreationTools extends Application {
         /**Check that there are no repeats */
         for (var x = 0; x < listValues.length; x++) {
             for (var y = 0; y < listValues.length; y++) {
+                if(!listValues[x]) {
+                    return true;
+                }
                 if (listValues[x] == listValues[y] && x != y){
                     return true;
                 }
@@ -249,43 +252,27 @@ function togglePointBuyScore(isPointBuy) {
 
 function changeAbility(i, up) {
     let stat = document.getElementById("number"+i);
-    let value = stat.valueAsNumber;
-    let cost = 1;
-    if(up && value > 12)
-        cost++;
-    else if(!up && value > 13)
-        cost++;
-
+    const value = stat.valueAsNumber;
     const isPointBuy = $('#point-buy-score').is(":visible");
-
-    const currentPointsElement = document.getElementById("point-buy-current-score");
-    const currentPoints = parseInt(currentPointsElement.innerHTML);
-    const maxPoints = parseInt(document.getElementById("point-buy-max-score").innerHTML);
-   
-    const newPoints = up ? currentPoints + cost : currentPoints - cost;
     const newValue = value + (up ? 1 : -1);
 
-    if(isPointBuy){
-        const disableUps = newPoints >= maxPoints;
-        console.log(`i: ${i} - points: ${newPoints}/${maxPoints} - disableUps: ${disableUps} - newValue: ${newValue}`)
-        for(let j=0; j<6; j++) {
+    stat.value = newValue;
 
-            if(j+1 != i){
-                $("#up"+(j+1)).prop( "disabled", disableUps );
-            }
-        }
-        
-        if(newValue == 15) {
-            $("#up"+i).prop("disabled", true);
-            $("#down"+i).prop("disabled", false);
-        } else if(newValue == 8) {
-            $("#up"+i).prop("disabled", false);
-            $("#down"+i).prop("disabled", true);
-        } else {
-            $("#up"+i).prop("disabled", disableUps);
-            $("#down"+i).prop("disabled", false);
-        }
+    if(isPointBuy) {
+        const cost = (up && value > 12) || (!up && value > 13) ? 2 : 1;
+        const currentPointsElement = document.getElementById("point-buy-current-score");
+        const currentPoints = parseInt(currentPointsElement.innerHTML);
+        const maxPoints = parseInt(document.getElementById("point-buy-max-score").innerHTML);
+        const newPoints = up ? currentPoints + cost : currentPoints - cost;
+        currentPointsElement.innerHTML = newPoints;
 
+        for(let j=1; j<7; j++) {
+            const v = document.getElementById("number"+j).valueAsNumber;
+            const disableUp = (newPoints >= maxPoints) || (v == 15);
+            const disableDown = (v == 8);
+            $("#up"+j).prop("disabled", disableUp);
+            $("#down"+j).prop("disabled", disableDown);
+        }
         // TODO do this on a notification? l18n?
         if(newPoints > maxPoints)
             alert("You have gone over the maximum points allowed for Point Buy");
@@ -299,9 +286,6 @@ function changeAbility(i, up) {
             $("#down"+i).prop("disabled", false);
         }
     }
-
-    currentPointsElement.innerHTML = newPoints;
-    stat.value = newValue;
     updateAbilityModifiers();
 }
 
